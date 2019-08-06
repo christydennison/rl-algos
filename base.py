fixed bad logprob, more to do

base.py
@@ -5,6 +5,7 @@ import os
 import collections
 import random
 import numpy as np
+import mujoco_py
 
 
 Transition = collections.namedtuple("Transition", ["old_obs", "new_obs", "action", "reward", "done", "log_prob"])
@@ -44,11 +45,7 @@ class Net(torch.nn.Module):
         self.load_state_dict(model.get_params())
         self.eval()
 
-    # def save(self, path):
-    #     torch.save(self.state_dict(), path)
-
     def forward(self, *inp):
-        # [i.float() for i in inp]
         return self.seq(*inp)
 
 
@@ -72,7 +69,7 @@ class ReplayBuffer():
             acts.append(transition.action.unsqueeze(0))
             rews.append(torch.tensor(transition.reward))
             dones.append(torch.tensor(transition.done))
-            log_probs.append(torch.tensor(transition.log_prob).unsqueeze(0))
+            log_probs.append(transition.log_prob.detach().unsqueeze(0))
         return torch.cat(old_obs).float(), torch.cat(new_obs).float(), torch.cat(acts).float(), torch.tensor(rews).unsqueeze(1).float(), torch.tensor(dones).unsqueeze(1), torch.cat(log_probs).float()
 
     def sample(self, batch_size):
@@ -102,7 +99,7 @@ class Agent():
         self.max_ep_len = self.args.steps_per_epoch or self.env.spec.max_episode_steps
         print("Max Episode Length", self.max_ep_len)
         self.replay_buffer = ReplayBuffer(maxsize=args.replay_size)
-        self.started = False
+        self.started = False  # this only flips once per lifetime of Agent
 
     def get_env_dims(self):
         return self.env.observation_space.shape[0], self.env.action_space.shape[0]
@@ -114,7 +111,10 @@ class Agent():
         while not(done or steps >= self.max_ep_len):
             action, log_prob = self.act_fn(old_obs, random=not self.started and self.args.start_steps > steps)
 
-            new_obs, reward, done, _ = self.env.step(action.detach().numpy())
+            try:
+                new_obs, reward, done, _ = self.env.step(action.detach().numpy())
+            except mujoco_py.builder.MujocoException:
+                break
 
             self.replay_buffer.add(Transition(old_obs, new_obs, action, reward, done, log_prob))
             steps += 1
@@ -130,6 +130,7 @@ class Agent():
         return steps
 
     def test(self, steps=1000):
+        self.env = gym.wrappers.Monitor(self.env, self.args.dir, force=True)
         observation = self.env.reset()
         total_reward = 0
         for _ in range(steps):
@@ -186,14 +187,19 @@ def train_base(args):
     return env, act_limit, obs_dim, act_dim
 
 
+def get_filenames(args):
+    logdir = os.path.join(args.dir, args.exp_name + "_data.csv")
+    paramsdir = os.path.join(args.dir, args.exp_name + "_params.mdl")
+    return logdir, paramsdir
+
+
 def base_argparser(*moreargs):
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("env_name", type=str)
     parser.add_argument("--exp_name", type=str)
-    parser.add_argument("--logdir", type=str, default=None)
-    parser.add_argument("--paramsdir", type=str)
+    parser.add_argument("--dir", type=str, default=None)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--epochs", type=int, default=100)
     parser.add_argument("--steps_per_epoch", type=int, default=5000)
@@ -205,6 +211,4 @@ def base_argparser(*moreargs):
     parser.add_argument("--start_steps", type=int, default=0)
     parser.add_argument("--test", action="store_true")
 
-
     return parser
-

ppo.py
@@ -23,13 +23,15 @@ def act(pi, act_limit, obs, deterministic=False):
 def cumulative_sum(data, discount):
     longest_T = len(max(data, key=len))
     discounts = torch.tensor([discount**i for i in range(longest_T)])
-    discounted = []
+    traj_discounted = []
     for datum in data:
+        discounted = []
         for t in range(len(datum)):
             to_end = datum[t:]
             discount_slice = discounts[:len(to_end)]
             discounted.append(torch.sum(discount_slice * to_end))
-    return torch.tensor(discounted)
+        traj_discounted.append(discounted)
+    return torch.tensor(traj_discounted)
 
 
 def reward_to_go(args, rewards):
@@ -63,7 +65,8 @@ def train(args):
         return act(pi, act_limit, torch.tensor(obs).float(), deterministic=deterministic)
 
     agent = Agent(args, env, curried_act)
-    log = DataLogger(args.logdir)
+    logfile, paramsfile = get_filenames(args)
+    log = DataLogger(logfile)
 
     start = time.time()
     v_mse_loss = torch.nn.MSELoss()
@@ -82,6 +85,7 @@ def train(args):
             trajectories.append(agent.replay_buffer.get())
             agent.replay_buffer.clear()
 
+        pi_current_params = pi.state_dict()
         for _ in range(args.train_iters):
             obs = torch.cat([traj[0] for traj in trajectories])
             obs_sp = torch.cat([traj[1] for traj in trajectories])
@@ -118,6 +122,9 @@ def train(args):
 
             epoch_rews.append(rewards.numpy())
 
+        # set to last pi's params
+        pi_prev.load_state_dict(pi_current_params)
+
         ep_rew = np.array(epoch_rews)
         log.log_tabular("ExpName", args.exp_name)
         log.log_tabular("AverageReturn", ep_rew.mean())
@@ -129,7 +136,7 @@ def train(args):
         log.log_tabular("Epoch", epoch)
         log.dump_tabular()
 
-    torch.save(pi, args.paramsdir)
+    torch.save(pi, paramsfile)
     agent.done()
 
 
@@ -140,7 +147,8 @@ def test(args):
     act_limit = env.action_space.high[0]
     pi = Net(obs_dim, act_dim * 2)  ## mean and std output
     pi.eval()
-    torch.load(args.paramsdir)
+    _, paramsfile = get_filenames(args)
+    torch.load(paramsfile)
     def curried_act(obs, random=False, deterministic=True):
         return act(pi, act_limit, torch.tensor(obs).float(), deterministic=True)
     agent = Agent(args, env, curried_act)
@@ -165,10 +173,7 @@ def main():
     if args.test:
         test(args)
     else:
-        if not args.logdir:
-            print("ERROR! Must have logdir specified")
-        else:
-            train(args)
+        train(args)
 
 
 if __name__ == "__main__":

sac.py
@@ -9,27 +9,19 @@ def act(pi, act_limit, obs, deterministic=False):
     res = pi(obs)
     mu, log_std = torch.chunk(res, 2, dim=-1)
     if not deterministic:
-        # dist = torch.distributions.normal.Normal(mu, torch.exp(log_std))
-        # unsquashed_sample = dist.sample()
-        # sample = torch.tanh(unsquashed_sample)
-        # log_prob = dist.log_prob(unsquashed_sample)
         std = torch.exp(log_std)
         with torch.no_grad():
             unsquashed_sample = torch.normal(mean=mu, std=std)
         log_prob = 0.5 * torch.sum(((unsquashed_sample - mu) / std)**2 + 2 * log_std, dim=-1)
+
+        # from https://github.com/openai/jachiam-sandbox/blob/master/Standalone-RL/myrl/algos/sac_new/sac.py#L51
+        log_prob -= torch.sum(2*(np.log(2) - unsquashed_sample - torch.nn.functional.softplus(-2*unsquashed_sample)), dim=-1)
+
         sample = torch.tanh(unsquashed_sample)
     else:
         sample = np.tanh(mu)
-        log_prob = None  # not used
+        log_prob = torch.tensor(-0.7)  # 50%
     return act_limit * sample, log_prob
-
-
-# def get_log_probs(pi, acts, obs):
-#     res = pi.forward(obs)  # do we really need to recompute this?
-#     mu, log_std = torch.chunk(res, 2)
-#     dist = torch.distributions.normal.Normal(mu, torch.exp(log_std))
-#     samples = -2.0 * torch.cosh(acts)  # inverting tanh
-#     return dist.log_prob(samples)
 
 
 def train(args):
@@ -49,11 +41,12 @@ def train(args):
 
     def curried_act(obs, random=False, deterministic=False):
         if random:
-            return torch.tensor(np.random.uniform(-act_limit, act_limit, act_dim)).float(), None
+            return torch.tensor(np.random.uniform(-act_limit, act_limit, act_dim)).float(), torch.tensor(-0.7)
         return act(pi, act_limit, torch.tensor(obs).float(), deterministic=deterministic)
 
     agent = Agent(args, env, curried_act)
-    log = DataLogger(args.logdir)
+    logfile, paramsfile = get_filenames(args)
+    log = DataLogger(logfile)
 
     start = time.time()
     q_mse_loss = torch.nn.MSELoss()
@@ -66,6 +59,9 @@ def train(args):
         print(f"--------Epoch {epoch}--------")
         step = 0
         epoch_rews = []
+        entropy_bonuses = []
+        if epoch == 2:
+            import ipdb; ipdb.set_trace()
 
         while step < args.steps_per_epoch:
             steps = agent.run_trajectory()
@@ -113,19 +109,26 @@ def train(args):
                     v_targ_state_dict[name] = args.polyak * v_targ_state_dict[name] + (1 - args.polyak) * params
 
                 epoch_rews.append(rews.numpy())
+                entropy_bonuses.append(entropy_bonus.detach().numpy())
 
             ep_rew = np.array(epoch_rews)
+            ep_entropy_bonus = np.array(entropy_bonuses)
             log.log_tabular("ExpName", args.exp_name)
             log.log_tabular("AverageReturn", ep_rew.mean())
             log.log_tabular("StdReturn", ep_rew.std())
             log.log_tabular("MaxReturn", ep_rew.max())
             log.log_tabular("MinReturn", ep_rew.min())
+            log.log_tabular("EntropyBonus", ep_entropy_bonus.mean())
             log.log_tabular("Time", time.time() - start)
-            log.log_tabular("Steps", step * (1 + epoch))
+            log.log_tabular("Steps", epoch * args.steps_per_epoch + step)
             log.log_tabular("Epoch", epoch)
+            # log out entropy
+            # plot out action values
+            # not working? bump up batch size
+            # nuts and bolts talk
             log.dump_tabular()
 
-    pi.save(args.paramsdir)
+    torch.save(pi, paramsfile)
     agent.done()
 
 
@@ -136,7 +139,8 @@ def test(args):
     act_limit = env.action_space.high[0]
     pi = Net(obs_dim, act_dim * 2)  ## mean and std output
     pi.eval()
-    torch.load(args.paramsdir)
+    _, paramsfile = get_filenames(args)
+    torch.load(paramsfile)
     def curried_act(obs, random=False, deterministic=True):
         return act(pi, act_limit, torch.tensor(obs).float(), deterministic=True)
     agent = Agent(args, env, curried_act)
@@ -157,10 +161,7 @@ def main():
     if args.test:
         test(args)
     else:
-        if not args.logdir:
-            print("ERROR! Must have logdir specified")
-        else:
-            train(args)
+        train(args)
 
 
 if __name__ == "__main__":